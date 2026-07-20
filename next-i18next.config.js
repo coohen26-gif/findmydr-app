d56862ce2e6ca81e605e469a8dd4fdcfc3b4f823
David (MyClaude) <myclaudemanager@gmail.com>
2026-07-20 18:48:28 +0000

    WS2-step1: install next-i18next 15.4 + i18n config

---
+++ next-i18next.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['fr', 'en', 'ar'],
+    localeDetection: false,
+    localePath: path.resolve('./public/locales'),
+  },
+  fallbackLng: 'en',
+  supportedLngs: ['fr', 'en', 'ar'],
+  defaultNS: 'common',
+  react: { useSuspense: false },
+  reloadOnPrerender: false,
+};
